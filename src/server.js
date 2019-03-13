c072ec71756629a32e4afb46442b31b61063bf74
Crippy <marks-c1@email.ulster.ac.uk>
2019-03-13 22:28:21 +0000

    Added new folder structures, wip new config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 
 export const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
 
